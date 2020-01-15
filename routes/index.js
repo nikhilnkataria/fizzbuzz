6129edba3304e85e01c43f9b1f496f71298e0903
Nikhil Kataria <nikhilnkataria@gmail.com>
2020-01-15 21:07:22 +0530

    Making count as optional parameter

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,26 @@ const { check, validationResult } = require("express-validator");
 var router = express.Router();
 const Utils = require("../helpers/utils");
 
-router.get("/:count", [check("count").isInt({ min: 1 })], (req, res) => {
-  let status = 200;
-  const errors = validationResult(req);
+router.get("/:count?", [check("count").isInt({ min: 1 })], (req, res) => {
+	let status = 200;
+	const errors = validationResult(req);
 
-  // send error if input param is invalid
-  if (!errors.isEmpty()) {
-    status = 400;
-    let response = Utils.generateHttpResponse(
-      { errors: errors.array() },
-      status
-    );
-    return res.status(status).json(response);
-  }
+	// send error if input param is invalid
+	if (!errors.isEmpty()) {
+		status = 400;
+		let response = Utils.generateHttpResponse(
+			{ errors: errors.array() },
+			status
+		);
+		return res.status(status).json(response);
+	}
 
-  const { count } = { ...req.params };
-  let fizzBuzzArr = [...Array(parseInt(count))].map((key, index) =>
-    Utils.getFizzBuzz(index + 1)
-  );
+	const { count } = { ...req.params };
+	let fizzBuzzArr = [...Array(parseInt(count))].map((key, index) =>
+		Utils.getFizzBuzz(index + 1)
+	);
 
-  res.status(status).json(Utils.generateHttpResponse({ fizzBuzzArr }, status));
+	res.status(status).json(Utils.generateHttpResponse({ fizzBuzzArr }, status));
 });
 
 module.exports = router;
